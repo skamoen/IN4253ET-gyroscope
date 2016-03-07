7a6e98c18e70ca5b7da8d84031f6ed77917126b6
Sille <sille.kamoen@gmail.com>
2016-03-07 10:00:26 +0100

    Initial webservice project

---
+++ webservice/src/main/java/nl/tudelft/GyroscopeServiceApplication.java
@@ -0,0 +1,12 @@
+package nl.tudelft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyroscopeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyroscopeServiceApplication.class, args);
+	}
+}
