903aa06e68dacd47e62ad63dfe970adc11be6bb4
hendrahc <hendra.h2c.x@gmail.com>
2016-04-20 12:16:05 +0200

    test to a 4digit pin

--- WEKA_Learning/gyrolearn/src/gyrolearn/Gyrolearn.java
+++ WEKA_Learning/gyrolearn/src/gyrolearn/Gyrolearn.java
@@ -10,9 +10,11 @@ import weka.core.Instances;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import weka.classifiers.AbstractClassifier;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
+
 
 /**
  *
@@ -33,15 +35,16 @@ public class Gyrolearn {
     public static int margin_front = 10;
     public static int margin_back = 10;
     
+    public static ArrayList<String> classes = new ArrayList<>();
     
     
-    public static Instances loadDataset() throws IOException{
-        
-        ArrayList<String> classes = new ArrayList<>();
+    public static void defineClass(){
         classes.add("pin_"+1);
         classes.add("pin_"+3);
         classes.add("pin_"+9);
-        
+    }
+    
+    public static Instances createDataset(){
         ArrayList<Attribute> attr_list = new ArrayList<>();
         
         for(int i=0; i<300; ++i){
@@ -52,10 +55,16 @@ public class Gyrolearn {
         
         attr_list.add(Class_attr);
         
-        
         Instances dataset = new Instances("GyroData", attr_list, data_capacity);
         
         dataset.setClass(Class_attr);
+        
+        return dataset;
+    }
+    
+    public static Instances loadDataset() throws IOException{
+        
+        Instances dataset = createDataset();
         
         for(String cls: classes){
             File dir = new File("data/"+cls+"/");
@@ -145,6 +154,24 @@ public class Gyrolearn {
         }
         
         return output_list;
+    }
+    
+    public static String predictPin(AbstractClassifier classifier, Instances rawtest) throws Exception{
+        String pin = "";
+        ArrayList<Instance> datatest = preProcessTest(rawtest);
+        Instances dataset = createDataset();
+        int counter = 1;
+        for(Instance d: datatest){
+            d.setDataset(dataset);
+            double[] p= classifier.distributionForInstance(d);
+            System.out.print("digit "+counter+": "); counter++;
+            for(double dist: p){
+                System.out.print(dist+" ");
+            }
+            System.out.println("");
+            
+        }
+        return pin;
     }
 	
 }
--- WEKA_Learning/gyrolearn/src/gyrolearn/Main.java
+++ WEKA_Learning/gyrolearn/src/gyrolearn/Main.java
@@ -32,6 +32,7 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
         
+        Gyrolearn.defineClass();
         Instances dataset = Gyrolearn.loadDataset();
         
         AbstractClassifier classifier = new NaiveBayes();
@@ -41,8 +42,19 @@ public class Main {
         eval.crossValidateModel(classifier, dataset, 10, new Random());
         System.out.println("Estimated Accuracy: "+Double.toString(eval.pctCorrect()));
         
+        Instances test = Gyrolearn.loadData("data/test/pin1393.txt");
+        Gyrolearn.predictPin(classifier,test);
         
         
+        for(int i=0; i< dataset.size(); ++i){
+            Instance trn = dataset.get(i);
+            double[] p= classifier.distributionForInstance(trn);
+            System.out.print("correct class "+trn.stringValue(dataset.classAttribute())+": ");
+            for(double dist: p){
+                System.out.print((dist*100)+" ");
+            }
+            System.out.println("");
+        }
         
     }
 }
